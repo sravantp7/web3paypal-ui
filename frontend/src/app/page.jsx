a3e29de491b7293c15b6d5cb4ba6f0aae74cda12
Sravan T P <sravantp03@gmail.com>
2023-07-29 14:14:35 +0530

    created ui to show user token balance in dollars

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -6,32 +6,88 @@ import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import axios from "axios";
 import { Layout, Button } from "antd";
 
+import CurrentBalance from "@/components/CurrentBalance";
+
+// api used to fetch user details from paypal contract
+const NAME_BALANCE_API = "http://localhost:3001/api/NameandBalance/";
+const HISTORY_API = "http://localhost:3001/api/history/";
+const REQUESTS_API = "http://localhost:3001/api/requests/";
+
 export default function Home() {
   const { Header, Content } = Layout;
 
+  // wallet connection
   const { address, isConnected } = useAccount();
-  const { connect, connectors } = useConnect({
+  const { connect } = useConnect({
     connector: new MetaMaskConnector()
   });
-
   const { disconnect } = useDisconnect();
+
+  const [name, setName] = useState("");
+  const [balance, setBalance] = useState("");
+  const [dollars, setDollars] = useState("");
+  const [history, setHistory] = useState(null);
+  const [requests, setRequests] = useState(null);
 
   // disconnect wallet
   function disconnectWallet() {
     disconnect();
+    setName("");
+    setBalance("");
+    setDollars("");
+    setHistory(null);
+    setRequests(null);
   }
 
-  // fetch the name and balance of the connected wallet by using backend api
-  async function getNameAndBalance() {
+  // fetch the name,balance, history of the connected wallet by using backend api
+  async function getDetails() {
+    // fetching wallet requests
+    try {
+      let res = await axios.get(`${NAME_BALANCE_API}${address}`);
+      let data = res.data;
+  
+      // checking if name exist for this person
+      if (data.name[1]) {
+        setName(data.name[0]);
+      }
+      setBalance(data.balance);
+      setDollars(data.usdPrice);
+  
+      // fetching wallet history
+      res = await axios.get(`${HISTORY_API}${address}`);
+      data = res.data;
+      setHistory(data);
+
+      res = await axios.get(`${REQUESTS_API}${address}`);
+      data = res.data;
+      setRequests(data);
+    } catch (err) {console.error(err.message)}
 
   }
+
+  useEffect(() => {
+    if (!isConnected) return;
+    getDetails();
+  }, [isConnected])
 
   return (
     <main className="App">
       <Layout>
         <Header className="header">
           <div className="headerLeft">
-
+            {isConnected && (
+              <>
+                <div
+                  className="menuOption"
+                  style={{ borderBottom: "1.5px solid black" }}
+                >
+                  Summary
+                </div>
+                <div className="menuOption">Activity</div>
+                <div className="menuOption">{`Send & Request`}</div>
+                <div className="menuOption">Wallet</div>
+              </>
+            )}
           </div>
           {
           isConnected ? (
@@ -40,7 +96,22 @@ export default function Home() {
           }
         </Header>
         <Content className="content">
+          {
+            isConnected ? (
+              <>
+                <div className="firstColumn">
+                  <CurrentBalance dollars={dollars} />
+                </div>
+                <div className="secondColumn">
 
+                </div>
+              </>
+            ) : (
+              <div>
+
+              </div>
+            )
+          }
         </Content>
       </Layout>
     </main>
---
+++ frontend/src/components/CurrentBalance.jsx
@@ -0,0 +1,16 @@
+import { Card } from "antd";
+
+export default function CurrentBalance({dollars}) {
+    return (
+    <Card title="Current Balance" style={{ width: "100%" }}>
+        <div className="currentBalance">
+          <div style={{ lineHeight: "70px" }}>$ {dollars}</div>
+          <div style={{ fontSize: "20px" }}>Available</div>
+        </div>
+        <div className="balanceOptions">
+          <div className="extraOption">Swap Tokens</div>
+          <div className="extraOption">Bridge Tokens</div>
+        </div>
+      </Card>
+    );
+}
